0ab7660a786f189114c1f3a5d6091fe79f2d1187
Victor Fernandes <victorcorr@gmail.com>
2019-05-19 02:23:05 +0000

    add location hook and global storage and reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from "react";
 
+import "./styles/styles.scss";
+
 import Header from "./components/header/Header";
 
 import HomePage from "./pages/HomePage";
-import "./styles/styles.scss";
+
+import { StateProvider } from "./service";
+
+const initialState = {
+  location: { lat: null, lng: null }
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_LOCATION":
+      return {
+        ...state,
+        location: action.location
+      };
+
+    default:
+      return state;
+  }
+};
 
 const App = () => (
   <div className="App">
     <Header />
-    <HomePage />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <HomePage />
+    </StateProvider>
   </div>
 );
+
 export default App;
--- src/components/GeoButton/GeoButton.jsx
+++ src/components/GeoButton/GeoButton.jsx
@@ -2,14 +2,28 @@ import React from "react";
 
 import "./styles.scss";
 
+import { useStateValue } from "../../service";
+
 import { getPosition } from "./geolocation";
 
 const GeoButton = () => {
-  // const [] = useState();
+  const [{ location }, dispatch] = useStateValue();
+
+  const geolocate = async () => {
+    const location = await getPosition();
+    dispatch({
+      type: "UPDATE_LOCATION",
+      location
+    });
+  };
+
   return (
-    <button className="geo-button" onClick={getPosition}>
-      Ativar Geolocalização
-    </button>
+    <>
+      <button className="geo-button" onClick={geolocate}>
+        Ativar Geolocalização
+      </button>
+      <p>{location.lng}</p>
+    </>
   );
 };
 
